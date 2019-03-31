ef0bfdbcd7b2e4268a518f7cdf629653c7b1847a
John Pennypacker <jpennypacker@uri.edu>
2019-03-31 16:51:31 -0400

    adds an idleness rating to each entry

--- app-logger.sh
+++ app-logger.sh
@@ -2,6 +2,8 @@
 
 TIMESTAMP=$(date +%Y-%m-%dT%H:%M:%S)
 
+IDLENESS=$((`ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/ !{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000))
+
 APPLICATION=$(osascript -e 'tell application "System Events" to set app_name to name of first process whose frontmost is true')
 
 CURRENTPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
@@ -9,4 +11,4 @@ CURRENTPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 mkdir -p $CURRENTPATH/application-logs
 #logger -s "Error Message"
 
-echo "$APPLICATION, $TIMESTAMP" >> $CURRENTPATH/application-logs/process-`date +"%Y%m%d"`.txt
+echo "$APPLICATION, $TIMESTAMP, $IDLENESS" >> $CURRENTPATH/application-logs/process-`date +"%Y%m%d"`.txt
