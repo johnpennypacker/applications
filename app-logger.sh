6995ad684a660cc6578f3e564ac59ee96e4ec1f9
John Pennypacker <jpennypacker@uri.edu>
2019-03-31 15:10:51 -0400

    added gitignore

---
+++ app-logger.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# run every minute, install in crontab:
+# * * * * * /Users/jpennypa/Applications/life-logs/app-logger.sh
+
+TIMESTAMP=$(date +%Y-%m-%dT%H:%M:%S)
+
+APPLICATION=$(osascript -e 'tell application "System Events" to set app_name to name of first process whose frontmost is true')
+
+CURRENTPATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+mkdir -p $CURRENTPATH/application-logs
+#logger -s "Error Message"
+
+echo "$APPLICATION, $TIMESTAMP" >> $CURRENTPATH/application-logs/process-`date +"%Y%m%d"`.txt
